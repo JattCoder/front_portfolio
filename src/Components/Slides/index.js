f1a9ad84dae3d1b3e211a2f9c07b4e617697a4b8
Harmandeep Mand <harmandeepmand.hm@gmail.com>
2021-10-05 14:40:02 -0400

    update

--- src/Components/Slides/index.js
+++ src/Components/Slides/index.js
@@ -1,24 +1,48 @@
 import React, { useEffect, useState } from 'react';
+import Apps from '../Apps';
+import Work from '../Work';
 import Bio from '../Bio';
+import Education from '../Education';
+import Contant from '../Contant';
 
 const Slides = (props) => {
 
+    //['Apps', 'Work', 'Home', 'Education', 'Contact'];
     const { selection } = props;
+    const [pageOpacity, setPageOpacity] = useState(1);
+    const [pageSelection, setPageSelection] = useState(-1);
 
     const styles = {
         frame: {
             height: '100%',
             width: '100%',
-        }
+            opacity: pageOpacity
+        },
+        
+    }
+
+    const displayNewData = () => {
+        setTimeout(() => setPageOpacity(1),500);
+        setPageSelection(selection);
+    }
+
+    const hidePage = () => {
+        setTimeout(() => displayNewData(), 1000)
+        setPageOpacity(0);
     }
 
     useEffect(() => {
-        
+        hidePage();
     },[selection])
 
     return (
         <div style={styles.frame}>
-            <Bio />
+            {pageSelection === 0 ? <Apps /> :
+            pageSelection === 1 ? <Work /> :
+            pageSelection === 2 ? <Bio /> :
+            pageSelection === 3 ? <Education /> :
+            pageSelection === 4 ? <Contant />:
+            null}
         </div>
     )
 }
