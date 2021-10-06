ae5f133f84a56b5580f6dbca33adf6a512ca81db
Harmandeep Mand <harmandeepmand.hm@gmail.com>
2021-10-05 20:05:49 -0400

    update

--- src/Components/Work/index.js
+++ src/Components/Work/index.js
@@ -1,9 +1,46 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { getJobs, getLastJob } from '../../Helper/jobs';
+import Cs from './Cs';
+import Freelance from './freelance.png';
+
 
 const Work = () => {
+
+    const currentJob = useSelector(state => getLastJob(state));
+    const allJobs = useSelector(state => getJobs(state));
+    const styles = {
+        frame: {
+            height: '100%',
+            width: '100%',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            backgroundColor: 'red'
+        },
+        centeredContainer: {
+            height: '65%',
+            width: '70%',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+        },
+        noAppsText: {
+            color: 'white',
+            fontFamily: 'Montserrat',
+            fontWeight: 'bold',
+            width: '25%',
+            textAlign: 'center',
+        }
+    }
+
+    console.log('Jobs: ',allJobs)
+
     return (
-        <div>
-            
+        <div style={styles.frame}>
+            <div style={styles.centeredContainer}>
+
+            </div>
         </div>
     )
 }
