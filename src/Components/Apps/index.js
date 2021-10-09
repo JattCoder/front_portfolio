a4ec0ceb048a9f71340a6d27bbe414ada75e1675
Harmandeep Mand <harmandeepmand.hm@gmail.com>
2021-10-09 10:21:19 -0400

    update

--- src/Components/Apps/index.js
+++ src/Components/Apps/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import { useSelector } from 'react-redux';
 import { getAllProjects } from '../../Helper/projects';
 
@@ -14,6 +14,7 @@ const Apps = () => {
 
     const apps = [
         {
+            id: 1,
             name: 'App1',
             icon: '',
             shortDesc: 'Mobile App',
@@ -21,6 +22,7 @@ const Apps = () => {
             images: '',
         },
         {
+            id: 2,
             name: 'App2',
             icon: '',
             shortDesc: 'Mobile App',
@@ -28,6 +30,7 @@ const Apps = () => {
             images: '',
         },
         {
+            id: 3,
             name: 'Npm1',
             icon: '',
             shortDesc: 'NPM',
@@ -35,6 +38,7 @@ const Apps = () => {
             images: '',
         },
         {
+            id: 4,
             name: 'Npm2',
             icon: '',
             shortDesc: 'NPM',
@@ -44,6 +48,7 @@ const Apps = () => {
     ];
 
     //const apps = useSelector(state => getAllProjects(state));
+    const section = useRef('');
     const [selectedApp, setSelectedApp] = useState(-1);
     const styles = {
         frame: {
@@ -75,8 +80,9 @@ const Apps = () => {
             left: 0,
         },
         appBox: {
-            height: 180,
+            height: 330,
             width: 180,
+            marginTop: '10%',
             background: "linear-gradient(#D3D3D3,#808080)",
             marginLeft: '.5%',
             marginRight: '.5%',
@@ -88,7 +94,7 @@ const Apps = () => {
             MozTransition: "all 0.7s ease",
         },
         selectedApp: {
-            height: 250,
+            height: 370,
             width: 250,
             display: 'flex',
             justifyContent: 'center',
@@ -102,17 +108,28 @@ const Apps = () => {
             MozTransition: "all 0.7s ease",
         },
         nonSelectedApp: {
-            height: 150,
+            height: 270,
             width: 150,
+            marginTop: '5%',
             alignItems: 'center',
-            background: "linear-gradient(#808080,#808080)",
+            background: "linear-gradient(rgba(0,0,0,1),rgba(0,0,0,0.9), rgba(0,0,0,0.8), rgba(0,0,0,0.7))",
             marginLeft: '.5%',
             marginRight: '.5%',
-            boxShadow: "1px 3px 10px #818589",
+            boxShadow: "1px 3px 10px black",
             borderRadius: 4,
             transition: "all 0.7s ease",
             WebkitTransition: "all 0.7s ease",
             MozTransition: "all 0.7s ease",
+        },
+        breakerFrame: {
+            height: '30%',
+            width: '3%',
+            marginTop: '-17%',
+            marginLeft: '3%',
+            opacity: selectedApp !== -1 ? 0 : 1,
+            transition: "all 0.2s ease",
+            WebkitTransition: "all 0.2s ease",
+            MozTransition: "all 0.2s ease",
         }
     }
 
@@ -130,10 +147,23 @@ const Apps = () => {
         <div style={styles.noAppsLayout}><text style={styles.noAppsText}>Harmandeep's Applications will be available soon</text></div>
     )
 
+    const addBreaker = (app, index) => {
+        section.current = app.shortDesc
+        return (
+            <div style={styles.breakerFrame}>
+                <text style={{color: 'white', fontFamily: 'Montserrat', fontWeight: 'bold'}}>{app.shortDesc === 'NPM' ? 'Node Packages' : 'Mobile Apps'}</text>
+                {index !== 0 ? <div style={{height: '100%', width: '1%',background: "linear-gradient(white,white,white, white,rgba(255,255,255,0))", }} /> : null}
+            </div>
+        )
+    }
+
     const renderApp = (app, index) => (
-        <div onMouseOver={() => setSelectedApp(index)} onMouseLeave={() => setSelectedApp(-1)} style={boxStyles(index)}>
-            <text>{app.name}</text>
-        </div>
+        <>
+            {app.shortDesc !== section.current ? addBreaker(app, index) : null}
+            <div onMouseOver={() => setSelectedApp(app.id)} onMouseLeave={() => setSelectedApp(-1)} style={boxStyles(app.id)}>
+                <text>{app.name}</text>
+            </div>
+        </>
     )
 
     return (
