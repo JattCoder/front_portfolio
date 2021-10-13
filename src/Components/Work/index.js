6cf81bd872d7b1d62cb79c9e318d0341791819f8
Harmandeep Mand <harmandeepmand.hm@gmail.com>
2021-10-13 10:07:47 -0400

    update

--- src/Components/Work/index.js
+++ src/Components/Work/index.js
@@ -3,9 +3,12 @@ import { useSelector } from 'react-redux';
 import { getJobs, getLastJob } from '../../Helper/jobs';
 import Cs from './Cs';
 import Freelance from './freelance.png';
+import Walletifai from './walletifai.svg';
 
 
 const Work = () => {
+
+    const LongText = 'This is one long long long long long long long long long long long long long long long long long long long long long long long long long text';
 
     const currentJob = useSelector(state => getLastJob(state));
     const allJobs = useSelector(state => getJobs(state));
@@ -22,7 +25,7 @@ const Work = () => {
             width: '100%',
             display: 'flex',
             alignItems: 'center',
-            justifyContent: 'space-between',
+            justifyContent: 'center',
         },
         noAppsText: {
             color: 'white',
@@ -34,9 +37,22 @@ const Work = () => {
         workCell: {
             height: '35%',
             width: '40%',
+            marginTop: '-10%',
+        },
+        workImageContainer: {
+            height: 50,
+            width: 50,
+            marginRight: '3%',
+            marginBottom: '8.5%',
+            justifyContent: 'center',
+            alignItems: 'center',
+        },
+        workImage: {
+            height: '100%',
+            width: '100%',
         },
         workNameContainer: {
-            width: '50%',
+            width: '100%',
         },
         workName: {
             color: 'white',
@@ -46,39 +62,71 @@ const Work = () => {
         },
         workTitleContainer: {
             width: '80%',
-            height: '50%',
+            height: '100%',
+            marginBottom: '10%',
         },
         workTitle:{
             color: 'white',
             fontFamily: 'Montserrat',
             fontSize: 15,
         },
+        workDescContainer: {
+
+        },
+        workDesc: {
+
+        },
         bottomDotContainer: {
             width: '100%',
             height: '20%',
             display: 'flex',
             alignItems: 'center',
+            marginLeft: '1%',
+            marginTop: '6%',
         },
         bottomDot: {
             height: 10,
             width: 10,
+            background: '#D3D3D3',
+            borderRadius: 50,
+        },
+        lastDot: {
+            height: 15,
+            width: 15,
             background: 'red',
             borderRadius: 50,
+        },
+        line: {
+            width: '95%',
+            height: '1%',
+            background: 'white',
+            marginLeft: '2%',
         }
-
     }
-
-    console.log(allJobs)
 
     return (
         <div style={styles.frame}>
             <div style={styles.centeredContainer}>
-                {allJobs.map(job => {
+                {allJobs.map((job, index) => {
                     return(
                        <div style={styles.workCell}>
-                            <div style={styles.workNameContainer}><text style={styles.workName}>{job.name.replace('and Deli', '')}</text></div>
-                            <div style={styles.workTitleContainer}><text style={styles.workTitle}>{job.title}</text></div>
-                            <div style={styles.bottomDotContainer}><div style={styles.bottomDot}/></div>
+                            <div style={{display: 'flex', alignItems: 'center'}}>
+                                <div style={styles.workImageContainer}>
+                                    {index === 0 ? (<div style={{background: '#696969', height: 50, width: 50, justifyContent: 'center', alignItems: 'center', borderRadius: 2}}><text style={{color: 'white', fontFamily: 'Montserrat', fontWeight: 'bold', marginLeft: '5%', marginTop: '5%'}}>C's</text></div>) : 
+                                    <img style={styles.workImage} src={index === 0 ? null : index === 1 ? Freelance : Walletifai} alt={'Work'} />}
+                                </div>
+                                <div style={{height: '100%', width: '100%'}}>
+                                    <div style={styles.workNameContainer}><text style={styles.workName}>{job.name.replace('and Deli', '')}</text></div>
+                                    <div style={styles.workTitleContainer}><text style={styles.workTitle}>{job.title}</text></div>
+                                </div>
+                            </div>
+                            <div style={{width: '100%', height: '80%',}}>
+                                <text style={{color: 'white'}}>{job.responsibility}</text>
+                            </div>
+                            <div style={styles.bottomDotContainer}>
+                                <div style={index <= allJobs.length - 2 ? styles.bottomDot : styles.lastDot }/>
+                                {index <= allJobs.length - 2 ? <div style={styles.line}/> : null}
+                            </div>
                         </div>
                     )
                 })}
