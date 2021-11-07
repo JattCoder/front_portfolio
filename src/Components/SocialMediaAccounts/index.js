0af6801ab4d0b5a5abaf3760cf222a69d0384f24
Harmandeep Mand <harmandeepmand.hm@gmail.com>
2021-11-07 10:35:47 -0500

    update

--- src/Components/SocialMediaAccounts/index.js
+++ src/Components/SocialMediaAccounts/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { useSelector } from 'react-redux';
 import { getAllMedia } from '../../Helper/socialMedia';
 import { text, width, height } from '../../Helper/responsive';
@@ -6,10 +6,14 @@ import './styles.css';
 
 const SocialMediaAccounts = () => {
 
+    const [emailRef, setEmailRef] = useState(null);
+    const [gitHubRef, setGithubRef] = useState(null);
+    const [linkedInRef, setLinkedInRef] = useState(null);
     const socialMedia = useSelector(state => getAllMedia(state));
     const [animateEmail, setAnimateEmail] = useState(height(-10.5));
     const [animateGutHub, setAnimateGitHub] = useState(height(-10));
     const [animateLinkedIn, setAnimatedLinkedIn] = useState(height(-10.5));
+    const [emailLeft, setEmailLeft] = useState(width(-8.8));
     const [handleEmailStyles, setHandleEmailStyles] = useState({
         weight: 'normal',
         size: text(2.4),
@@ -25,8 +29,8 @@ const SocialMediaAccounts = () => {
 
     const styles = {
         frame: {
-            width: width(100),
-            height: height(50),
+            width: '100%',
+            height: '50%',
             position: 'absolute',
             bottom: 0,
             transition: "all 1s ease",
@@ -36,7 +40,7 @@ const SocialMediaAccounts = () => {
         linkedInFrame: {
             transform: 'rotate(270deg)',
             position: 'absolute',
-            right: width(-1.5),
+            right: linkedInRef ? -linkedInRef.offsetWidth/2+14 : -20,
             bottom: animateLinkedIn,
             transition: "all 1s ease",
             WebkitTransition: "all 1s ease",
@@ -46,7 +50,7 @@ const SocialMediaAccounts = () => {
         linkedInText: {
             color: 'white',
             fontFamily: 'Montserrat',
-            fontSize: handleLinkedInStyles.size,
+            fontSize: text(2.4),
             fontWeight: handleLinkedInStyles.weight,
             transition: "all 1s ease",
             WebkitTransition: "all 1s ease",
@@ -65,7 +69,7 @@ const SocialMediaAccounts = () => {
         gitHubText: {
             color: 'white',
             fontFamily: 'Montserrat',
-            fontSize: handleGitHubStyles.size,
+            fontSize: text(2.4),
             fontWeight: handleGitHubStyles.weight,
             transition: "all 0.7s ease",
             WebkitTransition: "all 0.7s ease",
@@ -74,7 +78,7 @@ const SocialMediaAccounts = () => {
         emailFrame: {
             transform: 'rotate(90deg)',
             position: 'absolute',
-            left: width(-7.6),
+            left: emailRef ? -emailRef.offsetWidth/2+15 : -125,
             bottom: animateEmail,
             transition: "all 0.7s ease",
             WebkitTransition: "all 0.7s ease",
@@ -84,7 +88,7 @@ const SocialMediaAccounts = () => {
         emailText: {
             color: 'white',
             fontFamily: 'Montserrat',
-            fontSize: handleEmailStyles.size,
+            fontSize: text(2.4),
             fontWeight: handleEmailStyles.weight,
             transition: "all 1s ease",
             WebkitTransition: "all 1s ease",
@@ -101,7 +105,7 @@ const SocialMediaAccounts = () => {
         } else if (profile === 'Email') {
             setHandleEmailStyles({
                 weight: 'bold',
-                size: text(3.2),
+                size: text(2.5),
             });
         } else {
             setHandleGitHubStyles({
@@ -115,7 +119,7 @@ const SocialMediaAccounts = () => {
         if (profile === 'LinkedIn'){
             setHandleLinkedInStyles({
                 weight: 'normal',
-                size: text(2.4),
+                size: text(1.4),
             });
         } else if (profile === 'Email') {
             setHandleEmailStyles({
@@ -132,12 +136,12 @@ const SocialMediaAccounts = () => {
 
     const animateProfiles = () => {
         setAnimateEmail(height(24));
-        setAnimateGitHub(width(-1.1));
+        setAnimateGitHub(emailRef ? -emailRef.offsetWidth/2+15 : -15);
         setAnimatedLinkedIn(height(8));
     }
 
     const updateSizes = () => {
-        console.log('Screen Resized: ')
+        setEmailLeft(width(-8.8));
     }
 
     useEffect(() => {
@@ -147,9 +151,9 @@ const SocialMediaAccounts = () => {
 
     return(
         <div style={styles.frame}>
-            <div class='links' onClick={() => window.open(socialMedia.linkedin)} onMouseEnter={() => mouseEntered('LinkedIn')} onMouseLeave={() => mouseLeft('LinkedIn')} style={styles.linkedInFrame}><text style={styles.linkedInText}>LinkedIn</text></div>
-            <div class='links' onMouseEnter={() => mouseEntered('Email')} onMouseLeave={() => mouseLeft('Email')} style={styles.emailFrame}><text style={styles.emailText}>{socialMedia.email}</text></div>
-            <div class='links' onClick={() => window.open(socialMedia.github)} onMouseEnter={() => mouseEntered('GitHub')} onMouseLeave={() => mouseLeft('GitHub')} style={styles.gitHubFrame}><text style={styles.gitHubText}>GitHub</text></div>
+            <div class='links' ref={(reff => setLinkedInRef(reff))} onClick={() => window.open(socialMedia.linkedin)} onMouseEnter={() => mouseEntered('LinkedIn')} onMouseLeave={() => mouseLeft('LinkedIn')} style={styles.linkedInFrame}><text style={styles.linkedInText}>LinkedIn</text></div>
+            <div class='links' ref={(reff => setEmailRef(reff))} onMouseEnter={() => mouseEntered('Email')} onMouseLeave={() => mouseLeft('Email')} style={styles.emailFrame}><text style={styles.emailText}>{socialMedia.email}</text></div>
+            <div class='links' ref={(reff => setGithubRef(reff))} onClick={() => window.open(socialMedia.github)} onMouseEnter={() => mouseEntered('GitHub')} onMouseLeave={() => mouseLeft('GitHub')} style={styles.gitHubFrame}><text style={styles.gitHubText}>GitHub</text></div>
         </div>
     )
 }
